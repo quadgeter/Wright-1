7a9c353b21fe563efd120af959defe4e34c32885
quadgeter <quadgeter@gmail.com>
2024-05-28 22:57:04 -0400

    contact button functionality

---
+++ website/app/static/js/buttons.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('.more_bt').click(function() {
+        // Scroll to the bottom of the page with smooth behavior
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: 'smooth'
+        });
+        return false; // Prevent default action
+    });
+});
+
+$(document).ready(function() {
+    $('.contact_bt').click(function() {
+        // Scroll to the bottom of the page with smooth behavior
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: 'smooth'
+        });
+        return false; // Prevent default action
+    });
+});
+
